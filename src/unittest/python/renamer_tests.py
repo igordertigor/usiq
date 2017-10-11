ed4d434939f5d0aa5c296a7cd60c56f8cf9fafc8
Ingo Fründ <mail@ingofruend.net>
2017-10-10 23:50:05 -0400

    renamer in tests

--- src/unittest/python/renamer_tests.py
+++ src/unittest/python/renamer_tests.py
@@ -1,11 +1,11 @@
 from unittest import TestCase, mock
 
-from usiq import rename
+from usiq import renamer
 
 
 class TestCreateFilename(TestCase):
 
     def test_simple_title(self):
         tags = {'title': 'ANY TITLE', 'artist': 'ANY ARTIST'}
-        fname = rename.create_filename(tags, '<artist> - <title>.mp3')
+        fname = renamer.create_filename(tags, '<artist> - <title>.mp3')
         self.assertEqual(fname, 'ANY ARTIST - ANY TITLE.mp3')
