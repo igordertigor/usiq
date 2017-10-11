7e22739bf3c72639c642296b3934d0072f3393e4
Ingo Fründ <mail@ingofruend.net>
2017-10-10 23:12:03 -0400

    renaming basics

---
+++ src/main/python/usiq/rename.py
@@ -0,0 +1,8 @@
+from . import parser
+
+
+def create_filename(tags, pattern):
+    fields = parser.get_fields(pattern)
+    for field in fields:
+        pattern = pattern.replace('<{}>'.format(field), tags[field])
+    return pattern
